b57305f84f598aded8a53d505d8cb166b5dc37a5
kdy1242 <dy12311229@gmail.com>
2021-08-27 10:19:03 +0900

    package

--- 2301/module.package/do.py
+++ 2301/module.package/do.py
@@ -1,3 +1,20 @@
 import my_module as mm
+
 mm.지원이()
 print(mm.date)
+
+import travel.gangneung as ga
+gp = ga.GangneungPackage()
+print(gp)
+
+from travel import gangneung
+gp = gangneung.GangneungPackage()
+print(gp)
+
+from travel import ilsan
+ilsan.이지()
+
+from travel import *
+ilsan.이지()
+gp = gangneung.GangneungPackage()
+print(gp)
