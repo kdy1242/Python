7c078e1c33da10f32293a0b4184aeb6754a8ac2f
kdy1242 <dy12311229@gmail.com>
2021-10-29 09:49:15 +0900

    tictactoe game engine show_board, change_turn, set

--- 2301/tictactoe/tictactoe_game_engine.py
+++ 2301/tictactoe/tictactoe_game_engine.py
@@ -5,23 +5,31 @@ class TictactoeGameEngine:
         self.turn = 'X'
 
     def show_board(self):
-        print(self.board)
+        for a in range(len(self.board)):
+            print(self.board[a], end=' ')
+            if a % self.SIZE == self.SIZE - 1:   # 중요함 기말때보라함
+                print()
 
     def set(self, row, col):
-        pass
+        self.board[self.SIZE*(row-1) + (col-1)] = self.turn
 
     def set_winner(self):
         # - 3줄
         # | 3줄
         # \
         # /
-        return self.turn
+
         # 비기는 조건: 다 채워졌을때 위의것에 해당안됐을때: self.board 에 '.'이 없는 상테
-        return 'd'      # draw
+
+        if len(player_turn['X']) + len(player_turn['O']) == 9:
+            return 'd'
+        return False
+
+
 
     def change_turn(self):
         # self.turn 'X'면 'O'로, 'O'면 'X'로 바꾸자
-        pass
+        self.turn = 'O' if self.turn == 'X' else 'X'
 
 
 if __name__ == '__main__':
